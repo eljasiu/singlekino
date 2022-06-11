656874eb89a1823c4df6bc10188e26b5ea34e2cd
eljasiu <johnbg@tlen.pl>
2022-06-11 10:36:19 +0200

    Add shows page

--- base/views.py
+++ base/views.py
@@ -1,5 +1,23 @@
-from django.http import HttpResponse
+import datetime
+
 from django.shortcuts import render
 
+from .models import Movie
+
 def index(request):
-    return HttpResponse("Hello, world!")
+    today = datetime.date.today()
+    return shows(request, today.year, today.month, today.day)
+
+def shows(request, year, month, day):
+    movies = Movie.objects.all()
+    for movie in movies:
+        movie.shows = movie.show_set.filter(date = datetime.date(year, month, day))
+    movies = filter(lambda m: m.shows, movies)
+
+    timeline = [datetime.date.today() + datetime.timedelta(n) for n in range(7)]
+
+    ctx = {
+        'movies': movies,
+        'timeline': timeline
+    }
+    return render(request, 'base/shows.html', ctx)
