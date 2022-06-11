965fadffe5563576c36ad00ab3b79ab77e2efe53
eljasiu <johnbg@tlen.pl>
2022-06-11 07:56:54 +0200

    Create models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    image = models.URLField()
+    filmweb = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+class Show(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+
+class Reservation(models.Model):
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    row = models.CharField(max_length=1)
+    seat = models.IntegerField()
